8b46af6d5f56ffd303c13ca87e677c842691d7b0
kaustubhpande73 <82426976+kaustubhpande73@users.noreply.github.com>
2022-04-19 19:41:27 -0500

    Update test_predict.py

--- Tests/test_predict.py
+++ Tests/test_predict.py
@@ -14,5 +14,27 @@ from sklearn.metrics.pairwise import cosine_similarity
 import argparse
 import Project2
 
+def test_predict():
 
+    data_file = 'yummly.json'
+    ingredients = ['eggs', 'soy', 'cheese', 'beef', 'flour','ham']
+
+    #Parse .json file into df
+    df = get_data(data_file)
+
+    #Splitting df into 30% test and 70% train dataset 
+    X = df['Ingredients']
+    Y = df['Cuisine']
+    Z = df['Id']
+    X_train, X_test, Y_train, Y_test, Z_train, Z_test = train_test_split(X,Y,Z,test_size = 0.3)     
+
+    #Get vectorizer and model
+    vector, clf = model(X_train, X_test, Y_train)
+    #vector = model(X_train, X_test, Y_train)
+#     print(ingredients)  
+    print()
+    #Predict cuisine from user input
+    pred, ipr = predict(ingredients, vector, clf)
+    
+    assert pred!=None
 
