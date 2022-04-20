e6cda0d025d1a1a020aa93334b6c5d994085221b
kaustubhpande73 <82426976+kaustubhpande73@users.noreply.github.com>
2022-04-19 19:34:43 -0500

    Create main.py

---
+++ project 2/main.py
@@ -0,0 +1,14 @@
+import json
+import pandas as pd
+import spacy
+from spacy.lang.en import English
+import numpy as np
+import json
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+nlp = spacy.load('en_core_web_lg')
+import re
+from sklearn.metrics.pairwise import cosine_similarity
+import argparse
